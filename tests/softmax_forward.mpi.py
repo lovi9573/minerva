1163b8d73e29d74e8d813a803c1de96fffac418e
lovi9573 <jesselovitt@u.boisestate.edu>
2015-06-29 17:24:22 -0600

    fixed bug in cpu softmaxforward

--- tests/softmax_forward.mpi.py
+++ tests/softmax_forward.mpi.py
@@ -7,15 +7,16 @@ from owl import conv
 class TestForiegnOps(unittest.TestCase):
     
     def test(self):
-        base = np.asarray([10.0,20.0,30.0,40.0])
+        base = np.asarray([40.0,20.0,30.0,10.0])
+        max = np.max(base)
         base = np.reshape(base, [1,1,1,4])
         owlarray = owl.from_numpy(base)
-        expected = np.exp(base)
+        expected = np.exp(base - max)
         expected = expected / np.sum(expected)
         test = conv.softmax(owlarray)
         print 'Expected\n',expected
         print "Actual\n",test.to_numpy()
-        self.assertTrue(np.array_equal(expected, test.to_numpy()))
+        self.assertTrue(np.allclose(expected, test.to_numpy()))
 
 
 if __name__ == "__main__":
