0c7d0533bbc86cf969a810883686c230ea5fa926
lovi9573 <jesselovitt@u.boisestate.edu>
2015-09-01 16:45:51 -0600

    placeholder functions for backprop on fpga

--- HTModels/src/conv_forward.cpp
+++ HTModels/src/conv_forward.cpp
@@ -1,6 +1,15 @@
 #include <Ht.h>
 using namespace Ht;
 
+
+size_t get64_bit_aligned(size_t size){
+	if (size % 8 != 0){
+		return (size/8+1)*8;
+	}
+	return size;
+}
+
+
 void conv_forward(void *input_q88_data, size_t num_img, size_t img_dim, size_t img_channels, size_t img_alloc,
 		 void *filters_q88_data, size_t num_filters, size_t filter_dim, size_t stride, size_t filter_alloc,
 		 void *output_q88_data, size_t output_alloc,
@@ -16,16 +25,21 @@ void conv_forward(void *input_q88_data, size_t num_img, size_t img_dim, size_t i
 		pAuUnits[unit] = new CHtAuUnit(pHtHif);
 	}
 
+	//Ensure that the allocation can be interpreted as uint64_t
+	size_t img_alloc_ht = get64_bit_aligned(img_alloc);
+	size_t filter_alloc_ht = get64_bit_aligned(filter_alloc);
+	size_t output_alloc_ht = get64_bit_aligned(output_alloc);
+
 	// Set up memory
-	uint64_t* ht_input_img_data = (uint64_t*)pHtHif->MemAlloc(img_alloc/4* sizeof(uint64_t));
-	uint64_t* ht_input_filter_data = (uint64_t*)pHtHif->MemAlloc(filter_alloc/4* sizeof(uint64_t));
-	uint64_t* ht_output_data = (uint64_t*)pHtHif->MemAlloc(output_alloc/4 * sizeof(uint64_t));
+	uint64_t* ht_input_img_data = (uint64_t*)pHtHif->MemAlloc(img_alloc_ht);
+	uint64_t* ht_input_filter_data = (uint64_t*)pHtHif->MemAlloc(filter_alloc_ht);
+	uint64_t* ht_output_data = (uint64_t*)pHtHif->MemAlloc(output_alloc_ht);
 	if(!ht_input_img_data || !ht_input_filter_data || !ht_output_data){
 		printf("HT MemAlloc failed.\n");
 		exit(1);
 	}
-	pHtHif->MemCpy(ht_input_img_data, input_q88_data, img_alloc/4 * sizeof(uint64_t));
-	pHtHif->MemCpy(ht_input_filter_data, filters_q88_data, filter_alloc/4 * sizeof(uint64_t));
+	pHtHif->MemCpy(ht_input_img_data, input_q88_data, img_alloc_ht);
+	pHtHif->MemCpy(ht_input_filter_data, filters_q88_data, filter_alloc_ht);
 
 	// avoid bank aliasing for performance
 	if (unitCnt > 16 && !(unitCnt & 1)) unitCnt -= 1;
@@ -56,8 +70,72 @@ void conv_forward(void *input_q88_data, size_t num_img, size_t img_dim, size_t i
 	}
 
 	//Copy results out.
-	pHtHif->MemCpy(output_q88_data, ht_output_data, output_alloc/4 * sizeof(uint64_t));
+	pHtHif->MemCpy(output_q88_data, ht_output_data, output_alloc);
+}
 
+void conv_backward_data_ht(void* top_diff, size_t top_alloc,
+							void* filter_data, size_t num_filters, size_t filter_dim, size_t stride, size_t filter_alloc,
+							void* bottom_diff, int bottom_width, int bottom_height, int bottom_channels, int bottom_samples, size_t bottom_alloc,
+							int frac_w ){
 
+	// Get interfaces
+	CHtHif *pHtHif = new CHtHif();
+	int unitCnt = pHtHif->GetUnitCnt();
+	printf("#AUs = %d\n", unitCnt);
+
+	CHtAuUnit ** pAuUnits = new CHtAuUnit * [unitCnt];
+	for (int unit = 0; unit < unitCnt; unit++){
+		pAuUnits[unit] = new CHtAuUnit(pHtHif);
+	}
+
+	//Ensure that the allocation can be interpreted as uint64_t
+	size_t top_alloc_ht = get64_bit_aligned(top_alloc);
+	size_t filter_alloc_ht = get64_bit_aligned(filter_alloc);
+	size_t bottom_alloc_ht = get64_bit_aligned(bottom_alloc);
+
+	// Set up memory
+	uint64_t* ht_input_top_data = (uint64_t*)pHtHif->MemAlloc(top_alloc_ht);
+	uint64_t* ht_input_filter_data = (uint64_t*)pHtHif->MemAlloc(filter_alloc_ht);
+	uint64_t* ht_bottom_data = (uint64_t*)pHtHif->MemAlloc(output_alloc_ht);
+	if(!ht_input_img_data || !ht_input_filter_data || !ht_output_data){
+		printf("HT MemAlloc failed.\n");
+		exit(1);
+	}
+	pHtHif->MemCpy(ht_input_top_data, top_diff, top_alloc_ht);
+	pHtHif->MemCpy(ht_input_filter_data, filter_data, filter_alloc_ht);
+
+	// avoid bank aliasing for performance
+	if (unitCnt > 16 && !(unitCnt & 1)) unitCnt -= 1;
+	printf("stride = %d\n", unitCnt);
+	fflush(stdout);
+
+	/*
+	 * TODO(jesselovitt): change for backward_data
+
+	// Initialize modules with messages
+	pHtHif->SendAllHostMsg(IMG_ADDR, (uint64_t)ht_input_img_data);
+	pHtHif->SendAllHostMsg(IMG_NUM, (uint64_t)num_img);
+	pHtHif->SendAllHostMsg(IMG_DIM, (uint64_t)img_dim);
+	pHtHif->SendAllHostMsg(IMG_CHANNELS, (uint64_t)img_channels);
+	pHtHif->SendAllHostMsg(FILTER_ADDR, (uint64_t)ht_input_filter_data);
+	pHtHif->SendAllHostMsg(FILTER_NUM, (uint64_t)num_filters);
+	pHtHif->SendAllHostMsg(FILTER_DIM, (uint64_t)filter_dim);
+	pHtHif->SendAllHostMsg(STRIDE, (uint64_t)stride);
+	pHtHif->SendAllHostMsg(OUT_ADDR, (uint64_t)ht_output_data);
+	pHtHif->SendAllHostMsg(FRACTION_WIDTH, (uint64_t)fraction_width);
+*/
+
+	// Send calls to units
+	for (int unit = 0; unit < unitCnt; unit++)
+		pAuUnits[unit]->SendCall_htmain(unit /*offset*/, unitCnt /*stride*/);
+
+	// Wait for returns
+	for (int unit = 0; unit < unitCnt; unit++) {
+		while (!pAuUnits[unit]->RecvReturn_htmain())
+			usleep(1000);
+	}
+
+	//Copy results out.
+	pHtHif->MemCpy(bottom_diff, ht_bottom_data, bottom_alloc);
 }
 
--- minerva/op/impl/fpga.cpp
+++ minerva/op/impl/fpga.cpp
@@ -50,59 +50,141 @@ void ConvForward(const DataList& inputs, const DataList& outputs, ConvForwardClo
 	element_t* filter_data = inputs[1].data_;
 	element_t* output_data = outputs[0].data_;
 	size_t img_numbers = inputs[0].size_.Prod();
-	size_t img_bytes = img_numbers*2;
+	size_t img_bytes = img_numbers*sizeof(element_t);
 	size_t filter_numbers = inputs[1].size_.Prod();
-	size_t filter_bytes = filter_numbers*2;
+	size_t filter_bytes = filter_numbers*sizeof(element_t);
 	size_t output_numbers = outputs[0].size_.Prod();
-	size_t output_bytes = output_numbers*2;
-	int frac_width = 0;
+	size_t output_bytes = output_numbers*sizeof(element_t);
 
 
-	//Ensure that the allocation can be interpreted as uint64_t
-	if (img_bytes % 8 != 0){
-		img_bytes = (img_bytes/8+1)*8;
-	}
-	if (filter_bytes % 8 != 0){
-		filter_bytes = (filter_bytes/8+1)*8;
-	}
-	if (output_bytes % 8 != 0){
-		output_bytes = (output_bytes/8+1)*8;
-	}
 
-	char *input_q88_data = (char *)malloc(img_bytes);
-	char *filter_q88_data = (char *)malloc(filter_bytes);
-	char *output_q88_data = (char *)malloc(output_bytes);
 
-	element_t2qxx(input_data, input_q88_data,img_numbers,16,FIXED_POINT_FRACTION_WIDTH);
-	element_t2qxx(filter_data, filter_q88_data,filter_numbers,16,FIXED_POINT_FRACTION_WIDTH);
-	/*
-	  closure.pad_height;
-	  int pad_width;
-	  closure.stride_vertical;
-	  int stride_horizontal;
-
-	conv_forward(input_q88_data, size_t num_img, size_t img_dim, size_t img_channels, size_t img_alloc,
-			 void *filters_q88_data, size_t num_filters, closure.pad_height, closure.stride_vertical, size_t filter_alloc,
-			 void *output_q88_data, size_t output_alloc,
-			 uint16_t fraction_width);
-	*/
 	printf("num_img: %d, img_dim: %d, img_channels: %d, num_filters: %d, filter_dim: %d, stride: %d",
 			inputs[0].size_[3], inputs[0].size_[1], inputs[0].size_[2],
 			inputs[1].size_[3], inputs[1].size_[1], closure.stride_vertical);
 
-	conv_forward(input_q88_data, inputs[0].size_[3], inputs[0].size_[1], inputs[0].size_[2], img_bytes,
-				 filter_q88_data, inputs[1].size_[3], inputs[1].size_[1], closure.stride_vertical, filter_bytes,
-				 output_q88_data, output_bytes,
-				 frac_width);
-
-	qxx2element_t(output_q88_data, output_data, output_numbers,16,FIXED_POINT_FRACTION_WIDTH);
-	for(size_t i = 0; i < output_numbers; i++){
-		printf("%f\n",output_data[i]);
-	}
-	free(input_q88_data);
-	free(filter_q88_data);
-	//free(output_q88_data);
+	conv_forward(input_data, inputs[0].size_[3], inputs[0].size_[1], inputs[0].size_[2], img_bytes,
+				filter_data, inputs[1].size_[3], inputs[1].size_[1], closure.stride_vertical, filter_bytes,
+				output_data, output_bytes,
+				FIXED_POINT_FRACTION_WIDTH);
 }
+
+void ConvBackwardData(const DataList& inputs, const DataList& outputs, ConvBackwardDataClosure& closure){
+	CHECK_EQ(inputs.size(), 2) << " Convolution #inputs are wrong.\n";
+	CHECK_EQ(outputs.size(), 1) << " Convolution #outputs are wrong.\n";
+
+	element_t* bottom_diff = outputs[0].data_;
+	Scale bottom_size = outputs[0].size_;
+	int bottom_column_stride = bottom_size[0];
+	int bottom_channel_stride = bottom_size[1]*bottom_size[0];
+	int bottom_image_stride = bottom_size[2]*bottom_size[1]*bottom_size[0];
+
+	element_t* filters = inputs[1].data_;
+	Scale filtersize = inputs[1].size_;
+	int filter_column_stride = filtersize[0];
+	int filter_channel_stride = filtersize[1]*filtersize[0];
+	int filter_element_stride = filtersize[2]*filtersize[1]*filtersize[0];
+
+	//element_t* bias = inputs[2].data_;
+
+	element_t* top_diff = inputs[0].data_;
+	Scale top_size = inputs[0].size_;
+	//int top_column_stride = top_size[0];
+	//int top_channel_stride = top_size[1]*top_size[0];
+	//int top_image_stride = top_size[2]*top_size[1]*top_size[0];
+
+	for(int i = 0; i < bottom_size.Prod(); i++){
+		bottom_diff[i] = 0.0f;
+	}
+
+
+    int pad_height = closure.pad_height;
+    int pad_width = closure.pad_width;
+    int stride_vertical = closure.stride_vertical;
+    int stride_horizontal = closure.stride_horizontal;
+
+    /*
+    printf("bottom: (%d,%d) padding: (%d,%d), filter: (%d,%d), top: (%d,%d)\n",
+    		bottom_size[0], bottom_size[1],
+			pad_width, pad_height,
+			filtersize[0], filtersize[1],
+			top_size[0], top_size[1]);
+*/
+    conv_backward_data_ht(top_diff, top_size,
+    						filters, filtersize[3], filtersize[1], closure.stride_vertical, filtersize,
+							bottom_diff, bottom_size[0], bottom_size[1], bottom_size[2], bottom_size[3], bottom_size,
+							FIXED_POINT_FRACTION_WIDTH)
+}
+
+/*
+ * NOTE: This implementation is not designed to be fast.  If you want that, use a GPU.  It is meant to provide feature completeness and the ability to develop on a GPU-less machine.
+ */
+void ConvBackwardBias(const DataList& inputs, const DataList& outputs, ConvBackwardBiasClosure& closure){
+	CHECK_EQ(inputs.size(), 1) << " Convolution #inputs are wrong.\n";
+	CHECK_EQ(outputs.size(), 1) << " Convolution #outputs are wrong.\n";
+
+	element_t* bottom_diff = outputs[0].data_;
+	Scale bottom_size = outputs[0].size_;
+	//int bottom_image_stride = bottom_size[0];
+	//cout << "convolution bias_diff size " << bottom_size.ToString() << "\n";
+
+	element_t* top_diff = inputs[0].data_;
+	Scale top_size = inputs[0].size_;
+	int top_column_stride = top_size[0];
+	int top_channel_stride = top_size[1]*top_size[0];
+	int top_image_stride = top_size[2]*top_size[1]*top_size[0];
+
+
+	for(int i = 0; i < top_size[2]; i++){
+		bottom_diff[i] = 0.0f;
+	}
+
+	//TODO: write for fpga
+}
+
+/*
+ * NOTE: This implementation is not designed to be fast.  If you want that, use a GPU.  It is meant to provide feature completeness and the ability to develop on a GPU-less machine.
+ */
+void ConvBackwardFilter(const DataList& inputs, const DataList& outputs, ConvBackwardFilterClosure& closure){
+	CHECK_EQ(inputs.size(), 2) << " Convolution #inputs are wrong.\n";
+	CHECK_EQ(outputs.size(), 1) << " Convolution #outputs are wrong.\n";
+
+	element_t* bottom = inputs[1].data_;
+	Scale bottom_size = inputs[1].size_;
+	int bottom_column_stride = bottom_size[0];
+	int bottom_channel_stride = bottom_size[1]*bottom_size[0];
+	int bottom_image_stride = bottom_size[2]*bottom_size[1]*bottom_size[0];
+
+	element_t* filter_diff = outputs[0].data_;
+	Scale filtersize = outputs[0].size_;
+	int filter_column_stride = filtersize[0];
+	int filter_channel_stride = filtersize[1]*filtersize[0];
+	int filter_element_stride = filtersize[2]*filtersize[1]*filtersize[0];
+
+	//element_t* bias = inputs[2].data_;
+
+	element_t* top_diff = inputs[0].data_;
+	Scale top_size = inputs[0].size_;
+	//int top_column_stride = top_size[0];
+	//int top_channel_stride = top_size[1]*top_size[0];
+	//int top_image_stride = top_size[2]*top_size[1]*top_size[0];
+
+
+	for(int i = 0; i < filtersize.Prod(); i++){
+		filter_diff[i] = 0.0f;
+	}
+
+
+    int pad_height = closure.pad_height;
+    int pad_width = closure.pad_width;
+    int stride_vertical = closure.stride_vertical;
+    int stride_horizontal = closure.stride_horizontal;
+
+    int topindex = -1  ;
+
+    //TODO: write for fpga
+}
+
 
 
 
