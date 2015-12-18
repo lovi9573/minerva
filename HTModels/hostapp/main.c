4a0ccd720fa9dded498295664e0be452b4da1f31
lovi9573 <jesselovitt@u.boisestate.edu>
2015-12-18 15:39:13 -0700

    Added tests. Refactored HT code

--- HTModels/hostapp/main.c
+++ HTModels/hostapp/main.c
@@ -2,177 +2,244 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../HTModels.h"
-/*
 
-extern void conv_forward(void *input_q88_data, size_t num_img, size_t img_dim, size_t img_channels, size_t img_alloc,
-						 void *filters_q88_data, size_t num_filters, size_t filter_dim, size_t stride, size_t filter_alloc,
-						 void *output_q88_data, size_t output_alloc,
-						 uint16_t fraction_width);
-
-extern void conv_backward_data_ht(void* top_diff, size_t top_alloc,
-		void* filter_data, size_t num_filters, size_t filter_dim, size_t stride, size_t filter_alloc,
-		void* bottom_diff, int bottom_width, int bottom_height, int bottom_channels, int bottom_samples, size_t bottom_alloc,
-		int frac_w );
-
-extern void ConvBackwardBias_ht(void* top_diff, size_t top_alloc, size_t top_column_stride, size_t top_channel_stride, size_t top_image_stride,
-		void* bottom_diff, size_t bottom_alloc, int channels,
-		int frac_w);
-*/
 void usage (char *);
 
-int main(int argc, char **argv)
-{
-	uint16_t FRACW = 8;
+size_t prod(size_t* s){
+	return s[0]*s[1]*s[2]*s[3];
+}
+
+
+void test_fwd(){
+	uint16_t FRACW = 0;
 
 	/*
-	 * Bottom dims
+	 * Top diff
 	 */
-	size_t num_img =1;
-	size_t img_dim = 17;
-	size_t img_channels = 2;
-	size_t img_size = num_img*img_dim*img_dim*img_channels;
-	size_t img_alloc = img_size;
-	if(img_alloc%4 != 0){
-		img_alloc = (img_alloc/4+1)*4;
-	}
-	int16_t input_q88_data[img_alloc];
+	int16_t top_expected_raw[] = {544};
+	size_t top_diff_dims[] = {1,1,1,1};
+	size_t top_diff_size = prod(top_diff_dims)*sizeof(int16_t);
+	int16_t top_produced_raw[prod(top_diff_dims)];
 
 	/*
-	 * Filter dims
+	 * Filter
 	 */
-
-	size_t num_filters = 2;
-	size_t filter_dim = 3;
-	size_t stride = 2;
-	size_t filter_size = num_filters*filter_dim*filter_dim*img_channels;
-	size_t filter_alloc = filter_size;
-	if(filter_alloc%4 != 0){
-		filter_alloc = ((filter_alloc+3)/4)*4;
-	}
+	int16_t filter_raw[] ={11,12,13,14,15,16,17,18};
+	size_t filter_dims[] = {2, 2, 2, 1};
+	size_t filter_size = prod(filter_dims)*sizeof(int16_t);
 
 	/*
-	 * Bias dims
+	 * Bottom
 	 */
-	size_t bias_size = num_filters;
-	size_t bias_alloc = bias_size;
-	if(bias_alloc%4 != 0){
-		bias_alloc = ((bias_alloc+3)/4)*4;
-	}
 
-
-	/*
-	 * Top dims
-	 */
-	size_t num_samples = num_img;
-	size_t top_dim = ((img_dim-filter_dim)/stride+1);
-	size_t top_channels = num_filters;
-	size_t top_size = num_samples*top_dim*top_dim*top_channels;
-	size_t top_alloc = top_size;
-	if(top_alloc%4 != 0){
-		top_alloc = ((top_alloc+3)/4 )*4;
-	}
+	int16_t bottom_raw[] = {1,2,3,4,5,6,7,8};
+	size_t bottom_dims[] = {2, 2, 2, 1};
+	size_t bottom_size = prod(bottom_dims)*sizeof(int16_t);
 
 	/*
 	 * other
 	 */
-	int pad_x = 3;
-	int pad_y = 3;
+	int stride = 1;
 
-	int16_t filters_q88_data[filter_alloc];
-	int16_t bias_data[bias_alloc];
-	int16_t top_diff_data[top_alloc];
+
+	conv_forward(bottom_raw, bottom_dims[3], bottom_dims[0], bottom_dims[2], bottom_size,
+			filter_raw, filter_dims[3], filter_dims[1],  stride, filter_size,
+			top_produced_raw, top_diff_size,
+			FRACW);
 
     int err_cnt = 0;
-
-    int16_t expected_output = (int16_t)((filter_dim*filter_dim*img_channels));
-
-	size_t output_elements = num_samples*top_dim*top_dim*top_channels;
-	if(output_elements%4 != 0){
-		printf("Implementation requires that\n"
-				"\tnum_img*((img_dim-filter_dim)/stride+1)*((img_dim-filter_dim)/stride+1)*num_filters\n"
-				"be a multiple of 4.\n"
-				"got %lu\n",output_elements);
-		exit(1);
-	}
-	size_t output_alloc = output_elements;
-	if(output_alloc%4 != 0){
-		output_alloc = ((output_alloc+3)/4)*4;
-	}
-	int16_t output_q88_data[output_alloc];
-
-
-    for (size_t i = 0; i < img_size; i++) {
-    	input_q88_data[i] = 1 << (FRACW-1);
-    }
-
-
-    for (size_t i = 0; i < filter_size; i++) {
-    	filters_q88_data[i] = (1 << (FRACW-1));
-    }
-
-/*
-    conv_forward(input_q88_data,  num_img,  img_dim,  img_channels, img_alloc,
-    			 filters_q88_data,  num_filters,  filter_dim,  stride, filter_alloc,
-    			 output_q88_data, output_alloc,
-				 FRACW);
-
-    printf("Conv done\n");
-
-    // check results
-    if(expected_output > (1<< (16 - (FRACW-2)))){
-    	printf("Expected Cieling\n");
-    	expected_output = 0x7FFF;
-    	//expected_output = 0x8000;
-    }else{
-    	expected_output = expected_output << (FRACW-2);
-    }
-    printf("====== Convolution Forward =====\n");
-    for (size_t i = 0; i < output_elements; i++) {
-			if (output_q88_data[i] != expected_output) {
-				printf("output[%llu] is %hx, should be %hx\n",
-				(long long)i, output_q88_data[i], expected_output);
-				err_cnt++;
-			}else{
-				//printf("output[%llu] is %llu!!!!\n",(long long)i, (long long)output_q88_data[i]);
-			}
-    }
-*/
-
-	/*
-	 * Backward bias
-	 */
-/*    printf("====== Convolution Backward Bias =====\n");
-    for(int i =0; i < (int)top_alloc; i++){
-    	top_diff_data[i] = 1;
-    }
-    expected_output = num_samples*top_dim*top_dim;
-
-    ConvBackwardBias_ht(top_diff_data, top_alloc,	top_dim, top_dim*top_dim, top_dim*top_dim*top_channels,
-    					bias_data, bias_alloc, bias_size,
-						FRACW);
-    for (size_t i = 0; i < bias_size; i++) {
-		if (bias_data[i] != expected_output) {
-			printf("output[%llu] is %hx, should be %hx\n",
-			(long long)i, bias_data[i], expected_output);
+    printf("====== Convolution Forward [End]=====\n");
+    for (size_t i = 0; i < prod( top_diff_dims); i++) {
+		if (top_expected_raw[i] != top_produced_raw[i]) {
+			printf("output[%llu] is %" PRId16", should be %"PRId16"\n",
+			(long long)i, top_produced_raw[i], top_expected_raw[i]);
 			err_cnt++;
 		}else{
 			//printf("output[%llu] is %llu!!!!\n",(long long)i, (long long)output_q88_data[i]);
 		}
 	}
-*/
-    conv_backward_data_ht(top_diff_data, top_alloc,
-    					  filters_q88_data, num_filters, filter_dim, stride, pad_x, pad_y, filter_alloc,
-						  input_q88_data, img_dim, img_dim, img_channels, num_img, img_alloc,
-						  FRACW );
+
+    printf("\t#Errors: %d\n",err_cnt);
 
 
     if (err_cnt)
 	printf("FAILED: detected %d issues!\n", err_cnt);
     else
 	printf("PASSED\n");
-
-    return err_cnt;
 }
+
+
+void test_back_data(){
+	uint16_t FRACW = 0;
+
+	/*
+	 * Top diff
+	 */
+	int16_t top_diff_raw[] = {1,2,3,4,5,6,7,8};
+	size_t top_diff_dims[] = {2,2,2,1};
+	size_t top_diff_size = prod(top_diff_dims)*sizeof(int16_t);
+
+
+
+	/*
+	 * Filter
+	 */
+	int16_t filter_raw[] = {11,12,13,14,15,16,17,18};
+	size_t filter_dims[] = {2, 2, 2, 1};
+	size_t filter_size = prod(filter_dims)*sizeof(int16_t);
+
+	/*
+	 * Bottom diff
+	 */
+
+	int16_t bottom_diff_expected_raw[] = {14,41,26,54,130,74,36,81,44,18,53,34,70,170,98,48,109,60};
+	size_t bottom_diff_dims[] = {3, 3, 2, 1};
+	size_t bottom_diff_size = prod(bottom_diff_dims)*sizeof(int16_t);
+	int16_t bottom_diff_produced_raw[prod(bottom_diff_dims)];
+
+
+
+	/*
+	 * other
+	 */
+	int pad_x = 0;
+	int pad_y = 0;
+	int stride = 1;
+
+
+    conv_backward_data_ht(top_diff_raw, top_diff_size,
+    					  filter_raw, filter_dims[2], filter_dims[0], stride, pad_x, pad_y, filter_size,
+						  bottom_diff_produced_raw, bottom_diff_dims[0], bottom_diff_dims[1], bottom_diff_dims[2], bottom_diff_dims[3], bottom_diff_size,
+						  FRACW );
+
+
+    int err_cnt = 0;
+    printf("====== Convolution Backward Data [End]=====\n");
+    for (size_t i = 0; i < prod(bottom_diff_dims); i++) {
+		if (bottom_diff_expected_raw[i] != bottom_diff_produced_raw[i]) {
+			printf("output[%llu] is %" PRId16", should be %"PRId16"\n",
+			(long long)i, bottom_diff_produced_raw[i], bottom_diff_expected_raw[i]);
+			err_cnt++;
+		}else{
+			//printf("output[%llu] is %llu!!!!\n",(long long)i, (long long)output_q88_data[i]);
+		}
+	}
+
+    printf("\t#Errors: %d\n",err_cnt);
+
+
+    if (err_cnt)
+	printf("FAILED: detected %d issues!\n", err_cnt);
+    else
+	printf("PASSED\n");
+}
+
+void test_back_bias(){
+	 printf("====== Convolution Backward Bias [Start]=====\n");
+	uint16_t FRACW = 0;
+
+	/*
+	 * Top diff
+	 */
+	int16_t top_diff_raw[] = {1,2,3,4,5,6,7,8};
+	size_t top_diff_dims[] = {2,2,2,1};
+	size_t top_diff_size = prod(top_diff_dims)*sizeof(int16_t);
+
+	/*
+	 * Bias dims
+	 */
+	int16_t bias_raw[] = {10,26};
+	size_t bias_dims[] = {1,1,2,1};
+	size_t bias_size = prod(bias_dims)*sizeof(int16_t);
+	int16_t bias_produced_raw[prod(bias_dims)];
+
+	conv_backward_bias_ht(top_diff_raw, top_diff_size, top_diff_dims[0],top_diff_dims[1],
+					    top_diff_dims[2],top_diff_dims[3],
+						bias_produced_raw, bias_size,
+						FRACW);
+
+    int err_cnt = 0;
+    printf("====== Convolution Backward Bias [End]=====\n");
+    for (size_t i = 0; i < prod(bias_dims); i++) {
+		if (bias_raw[i] != bias_produced_raw[i]) {
+			printf("output[%llu] is %" PRId16", should be %"PRId16"\n",
+			(long long)i, bias_produced_raw[i], bias_raw[i]);
+			err_cnt++;
+		}else{
+			//printf("output[%llu] is %llu!!!!\n",(long long)i, (long long)output_q88_data[i]);
+		}
+	}
+
+    printf("\t#Errors: %d\n",err_cnt);
+
+    if (err_cnt)
+	printf("FAILED: detected %d issues!\n", err_cnt);
+    else
+	printf("PASSED\n");
+}
+
+void test_back_filter(){
+	uint16_t FRACW = 0;
+
+	/*
+	 * Top diff
+	 */
+	int16_t top_diff_raw[] = {8,6,4,2};
+	size_t top_diff_dims[] = {2,2,1,1};
+	size_t top_diff_size = prod(top_diff_dims)*sizeof(int16_t);
+
+	/*
+	 * Filter
+	 */
+	int16_t filter_expected_raw[] = {306,286,246,226};
+	size_t filter_dims[] = {2, 2, 1, 1};
+	size_t filter_size = prod(filter_dims)*sizeof(int16_t);
+	int16_t filter_produced_raw[prod(filter_dims)];
+
+	/*
+	 * Bottom
+	 */
+
+	int16_t bottom_raw[] = {10,11,12,13,14,15,16,17,18};
+	size_t bottom_dims[] = {3, 3, 2, 1};
+	size_t bottom_size = prod(bottom_dims)*sizeof(int16_t);
+
+	/*
+	 * other
+	 */
+	int pad_x = 0;
+	int pad_y = 0;
+	int stride = 1;
+
+
+    conv_backward_filter_ht(top_diff_raw, top_diff_size, top_diff_dims[0],top_diff_dims[1],top_diff_dims[2],top_diff_dims[3],
+    						filter_produced_raw, filter_size, filter_dims[0], stride, pad_x, pad_y,
+							bottom_raw, bottom_size, bottom_dims[0],bottom_dims[1],bottom_dims[2],
+							FRACW );
+
+    int err_cnt = 0;
+    printf("====== Convolution Backward Filter [End]=====\n");
+    for (size_t i = 0; i < prod(filter_dims); i++) {
+		if (filter_expected_raw[i] != filter_produced_raw[i]) {
+			printf("output[%llu] is %" PRId16", should be %"PRId16"\n",
+			(long long)i, filter_produced_raw[i], filter_expected_raw[i]);
+			err_cnt++;
+		}else{
+			//printf("output[%llu] is %llu!!!!\n",(long long)i, (long long)output_q88_data[i]);
+		}
+	}
+
+    printf("\t#Errors: %d\n",err_cnt);
+
+
+    if (err_cnt)
+	printf("FAILED: detected %d issues!\n", err_cnt);
+    else
+	printf("PASSED\n");
+}
+
+
+
 
 // Print usage message and exit with error.
 void
@@ -182,3 +249,10 @@ usage (char* p)
     exit (1);
 }
 
+int main(int argc, char **argv)
+{
+	test_fwd();
+//	test_back_data();
+//	test_back_bias();
+//	test_back_filter();
+}
